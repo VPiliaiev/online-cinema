963df06f43d742d4f6c7032d5bc30de55fce9bc2
Valerii <valerapiliaev@gmail.com>
2026-03-19 13:13:05 +0200

    refactor(accounts): remove duplication in accounts schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,39 +3,36 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-class BaseEmailPasswordSchema(BaseModel):
-    email: EmailStr
-    password: str
-
-    model_config = {
-        "from_attributes": True
-    }
-
-    @field_validator("email")
-    @classmethod
-    def validate_email(cls, value):
-        return value.lower()
-
-    @field_validator("password")
-    @classmethod
-    def validate_password(cls, value):
-        return accounts_validators.validate_password_strength(value)
-
-
-class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
-    pass
-
-
 class PasswordResetRequestSchema(BaseModel):
     email: EmailStr
 
+    @field_validator("email")
+    @classmethod
+    def validate_email_field(cls, value: str) -> str:
+        return accounts_validators.validate_email(value)
 
-class PasswordResetCompleteRequestSchema(BaseEmailPasswordSchema):
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {"from_attributes": True}
+
+    @field_validator("email")
+    @classmethod
+    def validate_email_field(cls, value: str) -> str:
+        return accounts_validators.validate_email(value)
+
+
+class UserRegistrationRequestSchema(UserLoginRequestSchema):
+    @field_validator("password")
+    @classmethod
+    def validate_password_strength(cls, value: str) -> str:
+        return accounts_validators.validate_password_strength(value)
+
+
+class PasswordResetCompleteRequestSchema(UserRegistrationRequestSchema):
     token: str
-
-
-class UserLoginRequestSchema(BaseEmailPasswordSchema):
-    pass
 
 
 class UserLoginResponseSchema(BaseModel):
