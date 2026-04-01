986099b14ef500a7d68c07a4d00764b7521add72
Valerii <valerapiliaev@gmail.com>
2026-04-02 00:19:26 +0300

    feat(order): add OrderModel and OrderItemModel

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,3 +2,4 @@ from routes.movies import router as movie_router
 from routes.accounts import router as accounts_router
 from routes.profiles import router as profiles_router
 from routes.cart import router as cart_router
+from routes.order import router as order_router
